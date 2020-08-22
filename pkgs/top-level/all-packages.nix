178e269079307f34d529a5a9f33a41b482b1d0e1
Phillip Cloud <cloud@standard.ai>
2020-08-22 11:02:58 -0400

    tensorflow-lite: init at v2.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1946,6 +1946,8 @@ in
     cudaSupport = config.cudaSupport or false;
   };
 
+  tensorflow-lite = callPackage ../development/libraries/science/math/tensorflow-lite { };
+
   behdad-fonts = callPackage ../data/fonts/behdad-fonts { };
 
   bfetch = callPackage ../tools/misc/bfetch { };
