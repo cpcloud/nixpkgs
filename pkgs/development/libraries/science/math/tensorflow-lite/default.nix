dfb9aedc3d42f05e06d6ca4c9c97ebc6dce9a299
Phillip Cloud <cpcloud@gmail.com>
2021-06-01 08:31:13 -0400

    tensorflow-lite: update to v2.5.0

--- pkgs/development/libraries/science/math/tensorflow-lite/default.nix
+++ pkgs/development/libraries/science/math/tensorflow-lite/default.nix
@@ -1,6 +1,6 @@
 { stdenv
 , bash
-, eigen
+, abseil-cpp
 , fetchFromGitHub
 , fetchFromGitLab
 , fetchpatch
@@ -10,34 +10,21 @@
 , hostPlatform
 , lib
 , zlib
-, abseil-cpp
 , gmock
 }:
 let
-  tensorflow-commit = "d855adfc5a0195788bf5f92c3c7352e638aa1109";
-
-  tflite-eigen = eigen.overrideAttrs (attrs: {
-    version = "3.3.90";
-    src = fetchFromGitLab {
-      owner = "libeigen";
-      repo = "eigen";
-      rev = "b9362fb8f76fbba805b56afbc0f5de0a279631b5";
-      sha256 = "02pv5k82w2avzw51z4xd8vycy4j8iplk6yhc6n1v869lg2np53bw";
-    };
-    patches = [
-      ./eigen_include_dir.patch
-      (fetchpatch {
-        url = "https://raw.githubusercontent.com/tensorflow/tensorflow/${tensorflow-commit}/third_party/eigen3/gpu_packet_math.patch";
-        sha256 = "08aqlvhg25vv7pp5drdwgmm6b3zw8x1f66kxxfq7pky9lwh2g36r";
-      })
-    ];
-  });
+  tflite-eigen = fetchFromGitLab {
+    owner = "libeigen";
+    repo = "eigen";
+    rev = "3d9051ea84a5089b277c88dac456b3b1576bfa7f";
+    sha256 = "1y3f2jvimb5i904f4n37h23cv2pkdlbz8656s0kga1y7c0p50wif";
+  };
 
   gemmlowp-src = fetchFromGitHub {
     owner = "google";
     repo = "gemmlowp";
-    rev = "12fed0cd7cfcd9e169bf1925bc3a7a58725fdcc3";
-    sha256 = "1a27h0yay00ppjr7cm5lhpydd66a4c7phihhgiw1zsygaxkl2gy1";
+    rev = "fda83bdc38b118cc6b56753bd540caa49e570745";
+    sha256 = "1sbp8kmr2azwlvfbzryy1frxi99jhsh1nc93bdbxdf8zdgpv0kxl";
   };
 
   neon-2-sse-src = fetchFromGitHub {
@@ -62,39 +49,36 @@ let
   fp16-src = fetchFromGitHub {
     owner = "Maratyszcza";
     repo = "FP16";
-    rev = "febbb1c163726b5db24bed55cc9dc42529068997";
-    sha256 = "1ayrddk2zdkpzixvrlkvn4az2kx5jnivxhvffr8177yxjslrmbfw";
+    rev = "4dfe081cf6bcd15db339cf2680b9281b8451eeb3";
+    sha256 = "06a8dfl3a29r93nxpp6hpywsajz5d555n3sqd3i6krybb6swnvh7";
   };
+
+  ruy-src = fetchFromGitHub {
+    owner = "google";
+    repo = "ruy";
+    rev = "23633b37099b614a2f836ef012cafc8087fdb98c";
+    sha256 = "14k9hz6ss8qy8nsajk6lrq25f6qxrldxky31ijw0dpqnfnnswrx4";
+  };
+
+  cpuinfo-src = fetchFromGitHub {
+    owner = "pytorch";
+    repo = "cpuinfo";
+    rev = "5916273f79a21551890fd3d56fc5375a78d1598d";
+    sha256 = "0q6760xdxsg18acdv8vq3yrq7ksr7wsm8zbyan01zf2khnb6fw4x";
+  };
+
+  version = "2.5.0";
 in
 stdenv.mkDerivation {
   pname = "tensorflow-lite";
-  version = "2.2.0";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "tensorflow";
     repo = "tensorflow";
-    rev = tensorflow-commit;
-    sha256 = "0igh58rp8iwjnilxvmybf80qvxhzy4qi0036rc4ciqn51qy6bwxj";
+    rev = "v${version}";
+    sha256 = "1jdw2i1rq06zqd6aabh7bbm0avsg4pygnfmd7gviv0blhih9054l";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/tensorflow/tensorflow/commit/72cd947f231950d7ecd1406b5a67388fef7133ea.patch";
-      sha256 = "0hlz64nap6rbb0p06paxlb6dy702r5qhwghlzvwla3qm5w88rmw6";
-    })
-    (fetchpatch {
-      url = "https://github.com/tensorflow/tensorflow/commit/b66f1593e8c4331890dff94ada5e13fbb2ac893b.patch";
-      sha256 = "04fbncpwajjlqhrhzqi0xxmvf9pg2flgyzsjlk6jhnrhkdgm5gmw";
-    })
-    (fetchpatch {
-      url = "https://github.com/tensorflow/tensorflow/commit/ccbbc5846c8fadd8dfda27d1b4b47eeb496b0a78.patch";
-      sha256 = "05saq40qjws0lkl1zf9vn4v0yq929a8cf5wvvggjq34699fhjxbj";
-    })
-    (fetchpatch {
-      url = "https://github.com/tensorflow/tensorflow/commit/20a904017c374fe58d67028453648e96cc7afb93.patch";
-      sha256 = "1dk6kxwv25yq6kp9rr59wc7lnf6pq7a6ssqc15b5nvclxx3h701g";
-    })
-  ];
 
   buildInputs = [ zlib flatbuffers gmock ];
 
@@ -115,20 +99,19 @@ stdenv.mkDerivation {
     tar xzf ${fft2d-src} -C .
 
     # enter the vendoring lair of doom
+    ln -s ${ruy-src} ./ruy
     ln -s ${gemmlowp-src} ./gemmlowp
     ln -s ${neon-2-sse-src} ./neon_2_sse
     ln -s ${farmhash-src} ./farmhash
+    ln -s ${cpuinfo-src} ./cpuinfo
+    ln -s ${fp16-src} ./fp16
+    ln -s ${tflite-eigen} ./eigen
 
     # tensorflow lite is using the *source* of flatbuffers
     ln -s ${flatbuffers.src} ./flatbuffers
 
-    ln -s ${fp16-src} ./fp16
-
     # tensorflow lite expects to compile abseil into libtensorflow-lite.a
     ln -s ${abseil-cpp.src} ./absl
-
-    # custom eigen, because why not?
-    ln -s ${tflite-eigen}/include/eigen3 ./eigen
 
     popd
     popd
@@ -140,6 +123,11 @@ stdenv.mkDerivation {
       -I $PWD/tensorflow/lite/tools/make/downloads/absl \
       -I $PWD/tensorflow/lite/tools/make/downloads/fp16/include \
       -I $PWD/tensorflow/lite/tools/make/downloads/farmhash/src \
+      -I $PWD/tensorflow/lite/tools/make/downloads/ruy \
+      -I $PWD/tensorflow/lite/tools/make/downloads/cpuinfo \
+      -I $PWD/tensorflow/lite/tools/make/downloads/cpuinfo/src \
+      -I $PWD/tensorflow/lite/tools/make/downloads/cpuinfo/include \
+      -I $PWD/tensorflow/lite/tools/make/downloads/cpuinfo/deps/clog/include \
       -I $PWD/tensorflow/lite/tools/make/downloads/eigen"
 
     ${gnumake}/bin/make \
@@ -149,20 +137,23 @@ stdenv.mkDerivation {
       TARGET_TOOLCHAIN_PREFIX="" \
       all
   '';
+
   installPhase = ''
     mkdir "$out"
 
     # copy the static lib and binaries into the output dir
     cp -r ./tensorflow/lite/tools/make/gen/linux_${hostPlatform.uname.processor}/{bin,lib} "$out"
 
-    # copy headers into the output dir
-    find ./tensorflow/lite -type f -name '*.h'| while read f; do
-      chmod -x "$f"
-      install -D "$f" "$out/include/''${f/.\//}"
+    find ./tensorflow/lite -type f -name '*.h' | while read f; do
+      path="$out/include/''${f/.\//}"
+      install -D "$f" "$path"
+
+      # remove executable bit from headers
+      chmod -x "$path"
     done
   '';
 
-  meta = with; {
+  meta = with lib; {
     description = "An open source deep learning framework for on-device inference.";
     homepage = "https://www.tensorflow.org/lite";
     license = licenses.asl20;
